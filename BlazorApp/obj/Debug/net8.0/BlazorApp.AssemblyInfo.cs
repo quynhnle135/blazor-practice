1b9502117d250ccb4e2c176efe905f5581b8cfcf
Quinn Le <quynhnle135@gmail.com>
2024-10-16 10:53:45 -0400

    Created Roman To Decimal Converter page

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d7a061e1a54c340f29a1a7d0964af2942831bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd210511f7bb941c99614f84ead868b1fd209235")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
